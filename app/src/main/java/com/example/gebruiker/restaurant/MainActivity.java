b8bca289344f446a5156fffa5f7a96a029551d46
Wohesi <woutsingerling@gmail.com>
2017-11-13 14:53:10 +0100

    working version with lists

--- app/src/main/java/com/example/gebruiker/restaurant/MainActivity.java
+++ app/src/main/java/com/example/gebruiker/restaurant/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -79,17 +80,8 @@ public class MainActivity extends AppCompatActivity {
                 error.printStackTrace();
             }
         });
-
         // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest);
-
     }
 
-
-    public void appetizers(View view) {
-        Intent storyScreen = new Intent(this, appetizers.class);
-        startActivity(storyScreen);
-        finish();
-
-    }
 }
